a91eadaaa3db7324cdf95f567658d6557a5f2429
Derek Seaverson <drseaverson@gmail.com>
2022-11-01 16:21:58 -0400

    player replacement/update

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,47 +1,73 @@
 extends KinematicBody2D
 
+#player value handlers
 export var move_speed = 250
 export var bullet_speed = 1375
-export var fire_rate = 0.09
+export var fire_rate = 0.50
+var gun_flare_rate = 0.05
 export var player_health = 100
+export var pistol_mag_size = 12
 
+
+#loads bullet and gun flare
 var bullet = preload("res://Player/PlayerBullet.tscn")
 var gun_flare = preload("res://Player/PlayerGunFlare.tscn")
-var rng = RandomNumberGenerator.new()
+#var rng = RandomNumberGenerator.new() random number generator (if needed)
 var can_shoot = true
+var can_melee = true
+var is_reloading = false
 
 
 func _process(delta):
 	look_at(get_global_mouse_position())
-		
-	# primary bullet/gun flare handler
-	if (Input.is_action_pressed("shoot_primary")) and can_shoot:
-		rng.randomize()
-		var random_num = rng.randf_range(-5, 5)
-		var bullet_temp = bullet.instance()
-		var gun_flare_temp = gun_flare.instance() 
-		gun_flare_temp.position = $BulletPoint.get_global_position()
-		gun_flare_temp.rotation_degrees = rotation_degrees
-		get_tree().get_root().add_child(gun_flare_temp)
-		bullet_temp.position = $BulletPoint.get_global_position()
-		bullet_temp.rotation_degrees = rotation_degrees
-		bullet_temp.apply_impulse(Vector2(), Vector2(bullet_speed, 0).rotated(rotation))
-		get_tree().get_root().add_child(bullet_temp)
-		can_shoot = false
-		yield(get_tree().create_timer(fire_rate), "timeout")
-		gun_flare_temp.queue_free()
-		can_shoot = true
 	
+	#intializes shot and delay before next shot can be taken
+	if Input.is_action_just_pressed("shoot_primary") and can_shoot and pistol_mag_size > 0:
+		can_shoot = false
+		#set animation for player shoot pistol
+		_player_shoot()
+		pistol_mag_size -= 1;
+		yield(get_tree().create_timer(fire_rate), "timeout")
+		can_shoot = true
+		
+	if Input.is_action_just_pressed("reload") and pistol_mag_size < 12:
+		$AnimatedSprite.animation = "Player_Reload_Pistol"
+		pistol_mag_size = 12
 
+#player movement management
 func _physics_process(delta):
 	var direction = Vector2.ZERO
 	if Input.is_action_pressed("move_up"):
+		$AnimatedSprite.animation = "Player_Move_Pistol"
 		direction.y -= 1.0
 	if Input.is_action_pressed("move_down"):
+		$AnimatedSprite.animation = "Player_Move_Pistol"
 		direction.y += 1.0
 	if Input.is_action_pressed("move_left"):
+		$AnimatedSprite.animation = "Player_Move_Pistol"
 		direction.x -= 1.0
 	if Input.is_action_pressed("move_right"):
+		$AnimatedSprite.animation = "Player_Move_Pistol"
 		direction.x += 1.0
+	
+	#resets player animation to idle when not moving/shooting
+	if (Input.is_action_just_released("move_up") || Input.is_action_just_released("move_down") || Input.is_action_just_released("move_right") || Input.is_action_just_released("move_left")) and can_shoot and can_melee and !is_reloading:
+		$AnimatedSprite.animation = "Player_Idle_Pistol"
+	#controls diagonal movement normalization
 	direction = direction.normalized()
 	move_and_slide(direction * move_speed)
+
+
+func _player_shoot():
+	var bullet_temp = bullet.instance()
+	var gun_flare_temp = gun_flare.instance() 
+	gun_flare_temp.position = $BulletPoint.get_global_position()
+	gun_flare_temp.rotation_degrees = rotation_degrees
+	get_tree().get_root().add_child(gun_flare_temp)
+	bullet_temp.position = $BulletPoint.get_global_position()
+	bullet_temp.rotation_degrees = rotation_degrees
+	bullet_temp.apply_impulse(Vector2(), Vector2(bullet_speed, 0).rotated(rotation))
+	get_tree().get_root().add_child(bullet_temp)
+	#minor yield to display gun flare
+	yield(get_tree().create_timer(gun_flare_rate), "timeout")
+	gun_flare_temp.queue_free()
