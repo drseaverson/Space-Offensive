76a9cba5a3e0f58547999a5342fb0be959163aa8
Derek Seaverson <drseaverson@gmail.com>
2023-08-15 08:50:23 -0400

    barrel functionality and sounds additions

--- Classes/Attack.gd
+++ Classes/Attack.gd
@@ -1,7 +1,9 @@
 class_name Attack
 
+# assigns attack function numbers for damage distrobution between all characters
 var attack_damage : float = 10
-var knockback_force : float = 0 # potential later use of knockback on certain enemies
+var knockback_force : float = 0
+var knockback_direction : float = 0
 var attack_position : Vector2
 var attack_rotation : float = 0
 #any other information needed for attacking
--- Obstacles/Explosive Barrel/ExplosiveBarrel.gd
+++ Obstacles/Explosive Barrel/ExplosiveBarrel.gd
@@ -3,26 +3,43 @@ extends StaticBody2D
 var explosion_timer = 5
 var health = 40
 var attack = Attack.new()
+var blink = false
 
 var animated_flame = preload("res://Obstacles/Explosive Barrel/Animated Flame.tscn")
 
 func _ready():
-	attack.attack_damage = 75
+	attack.attack_damage = 50
 	attack.knockback_force = 50
 
 # checks for barrel takes damage
 func take_damage(damage : Attack):
-		#spawn flame at point of bullet
-		#spawn_flame(damage)
-		health -= damage.attack_damage
-		print("Barrel Took Damage : ", damage.attack_damage)
-		if health <= 0:
-			barrel_explosion()
-			print("Barrel explosion")
+	if (!blink):
+		blink = true
+		$ExplosionTimer.start()
+	#spawn flame at point of bullet
+	#spawn_flame(damage)
+	health -= damage.attack_damage
+	print("Barrel Took Damage : ", damage.attack_damage)
+	if health <= 0:
+		barrel_explosion()
+		print("Barrel explosion")
 
 func barrel_explosion():
-	# spawn explosion animation, call damage to player/enemies in range
-	# possible knockback force applied
+	$Explosion.play()
+	# makes sprite invisible and unable to collide while sound plays
+	$Sprite2D.visible = false
+	get_node("Collision").set_deferred("disabled", true)
+	# gets all bodies within explosive area
+	var bodies : Array = $ExplosiveArea2D.get_overlapping_bodies()
+	if !bodies.is_empty():
+		for body in bodies:
+			if body.is_in_group("player") or body.is_in_group("enemy"):
+				# calls damage function and applies knockback to players
+				# general knockback direction function (variable is held in Attack class)
+				# var heading = (b.global_transform.origin - a.global_transform.origin).normalized()
+				body.take_damage(attack)
+	# spawn explosion animation
+	await get_tree().create_timer(2).timeout
 	queue_free()
 	
 func spawn_flame(damage : Attack):
@@ -32,3 +49,6 @@ func spawn_flame(damage : Attack):
 	# reverses direction of attack to make flame face away from barrel
 	flame_temp.rotation_degrees = (180 - abs(damage.attack_rotation)) - 90
 	get_tree().get_root().add_child(flame_temp)
+
+func _on_explosion_timer_timeout():
+	barrel_explosion()
